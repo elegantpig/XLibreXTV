785ccf06ef36d5c0f2698b2a656db337bac6a19e
elegantpig <89407465+elegantpig@users.noreply.github.com>
2026-09-19 00:14:55 +0800

    Update customer_site.js
    add guangsu

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,6 +3,10 @@ const CUSTOMER_SITES = {
         api: 'https://hongniuzy2.com/api.php/provide/vod/from/hnm3u8',
         name: '红牛资源',
     }
+    guangsu: {
+        api: 'https://api.guangsuapi.com/api.php/provide/vod/from/gsm3u8',
+        name: '光速资源',
+    }    
 };
 
 // 调用全局方法合并
