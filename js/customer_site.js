14caafd705be30f0096bf0efb644fc427348b2ba
elegantpig <89407465+elegantpig@users.noreply.github.com>
2026-09-20 22:39:42 +0800

    Update customer_site.js
    modify sites list

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,7 +2,7 @@ const CUSTOMER_SITES = {
     hongniu: {
         api: 'https://hongniuzy2.com/api.php/provide/vod/from/hnm3u8',
         name: '红牛资源',
-    }，
+    },
     guangsu: {
         api: 'https://api.guangsuapi.com/api.php/provide/vod/from/gsm3u8',
         name: '光速资源',
